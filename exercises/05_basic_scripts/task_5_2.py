365fd8c393547ac657f1421727cdd06ca1bded58
nekhochukha <nekhochukha90@mail.ru>
2021-08-02 03:51:12 -0500

    Zadanie 5.2-5.2a

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -25,3 +25,34 @@ You can then take 8 bits of the binary mask using slices and convert them to dec
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 """
+
+net_addr_mask = input('Введите адрес сети в формате "10.1.1.0/24": ')
+#net_addr_mask = '10.1.1.0/20'
+mask_net = net_addr_mask.split('/')[-1]
+addr_net = net_addr_mask.split('/')[0]
+oct1, oct2, oct3, oct4 = addr_net.split('.')
+oct1 = int(oct1)
+oct2 = int(oct2)
+oct3 = int(oct3)
+oct4 = int(oct4)
+tmpl_network ='''
+Network:
+{0:<10d}{1:<10}{2:<10}{3:<10}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
+'''
+mask_b = int(mask_net) * '1' + (32-int(mask_net)) * '0'
+mask_oct1 = int(mask_b[0:8], 2)
+mask_oct2 = int(mask_b[8:16], 2)
+mask_oct3 = int(mask_b[16:24], 2)
+mask_oct4 = int(mask_b[24:32], 2)
+tmpl_mask ='''
+Mask:
+/{0}
+{1:<10d}{2:<10}{3:<10}{4:<10}
+{1:08b}  {2:08b}  {3:08b}  {4:08b}
+'''
+#
+#print(type(mask_net), type(addr_net))
+#print(mask_net, addr_net.split('.'))
+print(tmpl_network.format(oct1, oct2, oct3, oct4))
+print(tmpl_mask.format(mask_net, mask_oct1, mask_oct2, mask_oct3, mask_oct4))
--- exercises/05_basic_scripts/task_5_2a.py
+++ exercises/05_basic_scripts/task_5_2a.py
@@ -41,3 +41,44 @@ Then the network address will be the first 28 characters from bin_ip + 0000
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 
 """
+
+net_addr_mask = input('Введите адрес сети в формате "10.1.1.0/24": ')
+#net_addr_mask = '10.1.1.195/28'
+mask_net = net_addr_mask.split('/')[-1]
+addr_host = net_addr_mask.split('/')[0]
+#Разбиваем адрес на октеты
+oct1, oct2, oct3, oct4 = addr_host.split('.')
+#Преобразуем адрес в 32 битное значение
+addr_host_b = f'{int(oct1):08b}{int(oct2):08b}{int(oct3):08b}{int(oct4):08b}'
+#Преобразуем маску в 32 битное значение
+mask_b = int(mask_net) * '1' + (32-int(mask_net)) * '0'
+#Получаем адрес сети согласно маске (заменяем нулями биты в введенном адресе сверх маски)
+addr_net_b_from_mask = addr_host_b[0:int(mask_net)] + (32-int(mask_net)) * '0'
+#Получаем десятичные значения октетов адреса сети
+ipnet_oct1 = int(addr_net_b_from_mask[0:8], 2)
+ipnet_oct2 = int(addr_net_b_from_mask[8:16], 2)
+ipnet_oct3 = int(addr_net_b_from_mask[16:24], 2)
+ipnet_oct4 = int(addr_net_b_from_mask[24:32], 2)
+
+tmpl_network ='''
+Network:
+{0:<10d}{1:<10}{2:<10}{3:<10}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
+'''
+#Получаем десятичные значения октетов маски сети
+mask_oct1 = int(mask_b[0:8], 2)
+mask_oct2 = int(mask_b[8:16], 2)
+mask_oct3 = int(mask_b[16:24], 2)
+mask_oct4 = int(mask_b[24:32], 2)
+
+tmpl_mask ='''
+Mask:
+/{0}
+{1:<10d}{2:<10}{3:<10}{4:<10}
+{1:08b}  {2:08b}  {3:08b}  {4:08b}
+'''
+#
+#print(type(mask_net), type(addr_net))
+#print(mask_net, addr_net.split('.'))
+print(tmpl_network.format(ipnet_oct1, ipnet_oct2, ipnet_oct3, ipnet_oct4))
+print(tmpl_mask.format(mask_net, mask_oct1, mask_oct2, mask_oct3, mask_oct4))
